b19cb4b379beac1a411c2dc5deda309f5278fc87
nikusrotuslomatus <74095621+nikusrotuslomatus@users.noreply.github.com>
2021-10-19 21:18:16 +0300

    Update app_but_better.py

--- app_but_better.py
+++ app_but_better.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
+a=0
 class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
